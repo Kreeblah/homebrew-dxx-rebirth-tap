88cb76f1aba5dbf65f38bf909ba39f170a825c62
Kreeblah <kreeblah@gmail.com>
2021-10-25 14:14:14 -0700

    Fixed style errors

--- Formula/dxx-rebirth.rb
+++ Formula/dxx-rebirth.rb
@@ -1,20 +1,20 @@
 class DxxRebirth < Formula
-  desc "An enhanced source port of the Descent 1 and Descent ][ engines"
+  desc "Enhanced source port of the Descent 1 and Descent ][ engines"
   homepage "https://www.dxx-rebirth.com"
-  version "20211021"
   url "https://www.dxx-rebirth.com/download/dxx/rebirth/dxx-rebirth_#{version}-src.tar.xz"
+  version "20211021"
   sha256 "ec0588d9ed394eaa6230311311a2c3d3997e7697408a664fd057717d53e2db9c"
-  head "https://github.com/dxx-rebirth/dxx-rebirth.git"
   license "GPL-3.0-only"
+  head "https://github.com/dxx-rebirth/dxx-rebirth.git"
 
-  depends_on "scons" => :build
-  depends_on "pkg-config" => :build
   depends_on "dylibbundler" => :build
+  depends_on "pkg-config" => :build
+  depends_on "scons" => :build
+  depends_on "libpng"
+  depends_on "physfs"
   depends_on "sdl"
   depends_on "sdl_image"
   depends_on "sdl_mixer"
-  depends_on "libpng"
-  depends_on "physfs"
 
   def install
     system "scons", "macos_add_frameworks=False"
@@ -32,6 +32,6 @@ class DxxRebirth < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "D1X-Rebirth.app/Contents/MacOS/d1x-rebirth -h"
+    system "D1X-Rebirth.app/Contents/MacOS/d1x-rebirth", "-h"
   end
 end
